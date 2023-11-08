44953c7bc3b65170db15cd13f67096732fa9326e
Cedric Chang <cedric.chang00@gmail.com>
2023-11-08 17:33:04 +0100

    Case check

--- routes/booking.js
+++ routes/booking.js
@@ -2,19 +2,15 @@ var express = require('express');
 var router = express.Router();
 
 const BookedTrip = require('../models/bookedTrips');
-const booked = [];
 
 router.get('/', (req, res) => {
     BookedTrip.find().then(allBookedTrip => {
         if (allBookedTrip.length !== 0) {
-            for (let i = 0; i < allBookedTrip.length; i++) {
-                booked.push(allBookedTrip[i]);
-            }
             BookedTrip.deleteMany({}).then();
+            res.json({ result: true, booked: allBookedTrip });
         } else {
             res.json({ result: false, error: "No booked trips found" });
         }
-        res.json({ result: true, booked: booked });
     })
 })
 
