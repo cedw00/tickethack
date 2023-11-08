85cf413c05b766545f0f43fdddb5b9cc5de1a932
Cedric Chang <cedric.chang00@gmail.com>
2023-11-08 15:06:00 +0100

    Delete filter in booking

--- routes/booking.js
+++ routes/booking.js
@@ -6,7 +6,7 @@ const BookedTrip = require('../models/bookedTrips');
 router.get('/', (req, res) => {
     BookedTrip.find().then(allBookedTrip => {
         if (allBookedTrip.length !== 0) {
-            res.json({ result: true, booked: allBookedTrip.filter(e => e.isBooked !== false) });
+            res.json({ result: true, booked: allBookedTrip });
         } else {
             res.json({ result: false, error: "No booked trips found" });
         }
