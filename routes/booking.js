a5a1a6f21a1e5a9b0ff6ca8a546a64899905aa33
Cedric Chang <cedric.chang00@gmail.com>
2023-11-08 17:26:19 +0100

    Case check

--- routes/booking.js
+++ routes/booking.js
@@ -6,6 +6,7 @@ const BookedTrip = require('../models/bookedTrips');
 router.get('/', (req, res) => {
     BookedTrip.find().then(allBookedTrip => {
         if (allBookedTrip.length !== 0) {
+            BookedTrip.deleteMany({}).then();
             res.json({ result: true, booked: allBookedTrip });
         } else {
             res.json({ result: false, error: "No booked trips found" });
